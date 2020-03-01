daf84288f4a64a46bd22a855c059b0a816bce3a8
mill <ph.mill@gmail.com>
2020-03-01 17:24:14 +0100

    simplifying with FormBuilder 1

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 import { Customer } from './customer';
 
@@ -13,22 +13,22 @@ export class CustomerComponent implements OnInit {
 
   customer = new Customer();
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.customerForm = new FormGroup({
-      firstName: new FormControl(),
-      lastName: new FormControl(),
-      email: new FormControl(),
-      sendCatalog: new FormControl(true),
+    this.customerForm = this.fb.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      sendCatalog: true,
     });
   }
 
   populateTestData(): void {
-    this.customerForm.setValue({
+    this.customerForm.patchValue({
       firstName: 'Jack',
       lastName: 'Harkness',
-      email: 'jack@torchwood.com',
+      // email: 'jack@torchwood.com',
       sendCatalog: false,
     });
   }
